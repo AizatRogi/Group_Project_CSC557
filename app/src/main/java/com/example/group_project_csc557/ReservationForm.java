ab38e8e546d089acab06a6e16a1f5cc8c1ed68ff
qeemtan <qeemtan0612@gmail.com>
2023-07-19 00:50:12 +0800

    Commit

--- app/src/main/java/com/example/group_project_csc557/ReservationForm.java
+++ app/src/main/java/com/example/group_project_csc557/ReservationForm.java
@@ -1,14 +1,84 @@
 package com.example.group_project_csc557;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ReservationForm extends AppCompatActivity {
+
+    private EditText txtName;
+    private EditText txtEmail;
+    private EditText txtPhone;
+    private EditText txtDate;
+    private EditText txtTime;
+    private static TextView tvCreated; // static because need to be accessed by DatePickerFragment
+    private static Date createdAt; // static because need to be accessed by DatePickerFragment
+    private Context context;
+
+    /**
+     * Date picker fragment class
+     * Reference: https://developer.android.com/guide/topics/ui/controls/pickers
+     */
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+
+            // create a date object from selected year, month and day
+            createdAt = new GregorianCalendar(year, month, day).getTime();
+
+            // display in the label beside the button with specific date format
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            tvCreated.setText( sdf.format(createdAt) );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_form);
+
+        // store context
+        context = this;
+
+        // get view objects references
+        txtName = findViewById(R.id.txtName);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPhone = findViewById(R.id.txtPhone);
+        txtDate = findViewById(R.id.txtDate);
+        txtTime = findViewById(R.id.txtTime);
+        tvCreated = findViewById(R.id.tvCreated);
+
+        // set default createdAt value, get current date
+        createdAt = new Date();
+
+        // display in the label beside the button with specific date format
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        tvCreated.setText( sdf.format(createdAt));
     }
 }
